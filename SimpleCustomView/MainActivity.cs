536f9981a7eb77f9057109b432dd7c7bbb587066
José Augusto Mendes <zeca.saitou@gmail.com>
2019-03-15 07:39:50 -0300

    Simple Custom View Use
    This project is an example of the Xamarin Custom View use. Was implemented a Game Main Loop with Draw and Update Methods to the Game Objects.

--- SimpleCustomView/MainActivity.cs
+++ SimpleCustomView/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 
-namespace XamarinSimpleCustomView
+namespace SimpleCustomView
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
@@ -13,7 +13,7 @@ namespace XamarinSimpleCustomView
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(new MainView(this));
         }
     }
 }
---
+++ SimpleCustomView/MainView.cs
@@ -0,0 +1,72 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Util;
+using Android.Views;
+using Java.Lang;
+
+namespace SimpleCustomView
+{
+    public class MainView : View, IRunnable
+    {
+        Context c;
+        private Handler handler;
+        private bool isUpdating;
+        private int greenX;
+
+        private Paint green;
+
+        public MainView(Context context) : base(context)
+        {
+            Initialize(context);
+        }
+
+        public MainView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            Initialize(context);
+        }
+
+        public MainView(Context context, IAttributeSet attrs, int defStyle) : base(context, attrs, defStyle)
+        {
+            Initialize(context);
+        }
+
+        private void Initialize(Context context)
+        {
+            c = context;
+            isUpdating = true;
+
+            green = new Paint();
+            green.SetARGB(255, 0, 255, 0);
+            greenX = 100;
+
+            handler = new Handler();
+            handler.Post(this);
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+
+            //Aqui desenho meus objetos
+            canvas.DrawRect(greenX, 100, 50 + greenX, 50, green);
+        }
+
+        private void OnUpdate()
+        {
+            //Aqui atualizado meus objetos
+            greenX += 2;
+        }
+
+        public void Run()
+        {
+            if(isUpdating == true)
+            {
+                handler.PostDelayed(this, 30);
+
+                OnUpdate();
+                this.Invalidate();
+            }
+        }
+    }
+}
